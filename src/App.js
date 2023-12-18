e605f30e89bb6f4de1af446b85d807ed550c84b2
yasu2122yasu <ya2su1shi2@icloud.com>
2023-12-18 12:27:22 +0900

    make pull requeset finish 3-5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
-import { useSelector } from 'react-redux';
-import Count from './components/count';
+import { useSelector, useDispatch } from 'react-redux';
+import { increaseCount, decreaseCount } from './store/index'; // ここを追加
 
 function App() {
   // useSelector フックを使用して Redux の状態にアクセス
   const count = useSelector((state) => state.count.count);
+  const dispatch = useDispatch();
+  const increase = () => {
+    dispatch(increaseCount());
+  };
+  const decrease = () => {
+    dispatch(decreaseCount());
+  };
+
+  console.log(count);
 
   return (
     <div className="App">
       <h1>Redux Learn</h1>
       <p>Count: {count}</p>
-      <Count />
+      <button onClick={increase}>Up</button>
+      <button onClick={decrease}>Down</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,46 @@
-import { createStore, combineReducers } from 'redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const countReducer = (state = { count: 50 }) => {
-  return state;
+// 初期状態を定義
+const initialCountState = { count: 50 };
+const initialPostsState = {
+  posts: [
+    { id: 1, title: 'Reduxについて' },
+    { id: 2, title: 'ReduxのHooksについて' },
+  ],
 };
 
-const postsReducer = (
-  state = {
-    posts: [
-      { id: 1, title: 'Reduxについて' },
-      { id: 2, title: 'ReduxのHooksについて' },
-    ],
-  }
-) => {
-  return state;
-};
-
-const rootReducer = combineReducers({
-  count: countReducer, // 名前を変更
-  posts: postsReducer,
+// createSliceを使用してcountReducerを定義
+const countSlice = createSlice({
+  name: 'count',
+  initialState: initialCountState,
+  reducers: {
+    increaseCount: (state) => {
+      state.count += 1;
+    },
+    decreaseCount: (state) => {
+      state.count -= 1;
+    },
+  },
 });
 
-const store = createStore(rootReducer);
+// createSliceを使用してpostsReducerを定義
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState: initialPostsState,
+  reducers: {},
+});
+
+// rootReducerに組み込む
+const rootReducer = {
+  count: countSlice.reducer,
+  posts: postsSlice.reducer,
+};
+
+// ストアを設定
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+// ストアとアクションクリエーターをエクスポート
+export const { increaseCount, decreaseCount } = countSlice.actions;
 export default store;
