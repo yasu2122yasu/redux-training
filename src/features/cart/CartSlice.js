6f3746c07383b6bcf5c89e325aec95f349d92711
yasu2122yasu <ya2su1shi2@icloud.com>
2023-12-26 23:07:32 +0900

    make pull requeset

--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
 
 const initialState = {
-  cartItems: [],
+  cartItems: cartItems,
   amount: 4,
   total: 0,
 };
