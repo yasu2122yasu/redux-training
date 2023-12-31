662a8978d6f1ee903d114f17131a07ed79c4a718
yasu2122yasu <ya2su1shi2@icloud.com>
2023-12-31 23:41:11 +0900

    make pull requeset

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
+// useEffectは発火のタイミングを決めることができる。
+// cartの状態が変更されたときに、useEffectも変更できるようにする
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/CartSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  // useEffectは第二引数の配列に変更が加わった場合に、第一引数の関数を実行する。
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
